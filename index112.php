5e8c500a832229c6a34166a9c0122fcf427822ba
Mohsin Raza <mohsinraza.work@yahoo.com>
2016-02-26 06:32:01 +0500

    Google Autocomplete show places only in canada

--- index112.php
+++ index112.php
@@ -114,7 +114,7 @@ function initAutocomplete() {
     // location types.
     autocomplete = new google.maps.places.Autocomplete(
                                                        /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-                                                       {types: ['geocode']});
+                                                       {types: ['geocode'],componentRestrictions: {country: "ca"}});
     
     // When the user selects an address from the dropdown, populate the address
     // fields in the form.
